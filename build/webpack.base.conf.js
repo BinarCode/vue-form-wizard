435ee4261667a1ecba53afd0dec1fff212e864dc
cristijora <cristian.jora@corebuild.eu>
2017-04-18 21:10:45 +0300

    Reduce css size in half Move dev example to dev folder

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './dev/main.js'
   },
   output: {
     path: config.build.assetsRoot,
--- dev/main.js
+++ dev/main.js
@@ -1,9 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App.vue'
-import TabWizard from './../src/index';
-Vue.use(TabWizard);
+import FormWizard from './../src/index'
+Vue.use(FormWizard)
 
 Vue.config.productionTip = false
 
@@ -11,5 +9,5 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
-});
+  components: {App}
+})
