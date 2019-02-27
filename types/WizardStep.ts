9ab19be70f47c745241835a15910d8c356454624
Charles Salmon <charles.salmon@intranel.com>
2019-02-28 12:13:47 +1300

    Improve documentation on `WizardStep` type definitions, and order props in alignment iwth props declared in `WizardStep.vue`.

--- types/WizardStep.ts
+++ types/WizardStep.ts
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 
 export declare class Step extends Vue {
-  /** Tab component for the step. This should be usually passed through the `step` scoped slot */
+  /**
+   * Tab component to use for the step. This should be usually passed through the `step` scoped slot.
+   * 
+   * Default value: () => {}
+   */
   tab: object
+
+  /**
+   * Default value: ''
+   */
   transition: string
+
+  /**
+   * Default value: 0
+   */
   index: number
 }
