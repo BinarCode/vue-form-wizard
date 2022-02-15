e562fe2880cef9273959baf938acaabf5d09e172
Alicard <mralicard@gmail.com>
2022-02-15 19:33:17 +0100

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -14,10 +14,20 @@ import { Wizard as FormWizard } from "./FormWizard"
 import { Tab as TabContent } from "./TabContent"
 import { Step as WizardStep } from "./WizardStep"
 
+app.use(FormWizard, {
+  /* optional options */
+})
+
+app.use(TabContent, {
+  /* optional options */
+})
+
+app.use(WizardStep, {
+  /* optional options */
+})
+
 export default VueFormWizard {
   install: (app, options) => {
-    FormWizard: FormWizard;
-    TabContent: TabContent;
-    WizardStep: WizardStep;
+    
   }
 }
