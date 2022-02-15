e7db44ea4e9d98dffd9d42cf673759ecc4599d4a
Alicard <mralicard@gmail.com>
2022-02-15 19:34:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+import { createApp } from 'vue'
+
+const app = createApp({})
+
 import FormWizard from './components/FormWizard.vue'
 import TabContent from './components/TabContent.vue'
 import WizardButton from './components/WizardButton.vue'
 import WizardStep from './components/WizardStep.vue'
 const VueFormWizard = {
-  install (Vue) {
-    Vue.component('form-wizard', FormWizard)
-    Vue.component('tab-content', TabContent)
-    Vue.component('wizard-button', WizardButton)
-    Vue.component('wizard-step', WizardStep)
+  install (app) {
+    app.component('form-wizard', FormWizard)
+    app.component('tab-content', TabContent)
+    app.component('wizard-button', WizardButton)
+    app.component('wizard-step', WizardStep)
   }
 }
 // Automatic installation if Vue has been added to the global scope.
